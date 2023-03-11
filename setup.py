bb65620a5dc976bd61366e680819520e1cfcf7fc
swagatx1 <swagatrout02@gmail.com>
2023-03-12 01:13:05 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEM_E_DOT="-e ."
+def get_requirements(file_path:str)-> List[str]:
+    '''
+    This function will return the list of requirents
+    
+    '''
+    requirments=[]
+    with open(file_path) as file_obj:
+        requirments = file_obj.readline()
+        requirments = [req.replace("\n"," ") for req in requirments]
+        if HYPEM_E_DOT in requirments:
+            requirments.remove(HYPEM_E_DOT)
+   
+    return requirments
+        
+
+
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='swagat',
+author_email='swagatrout02@gmail.com',
+packages = find_packages(),
+install_requires=get_requirements('requirments.txt'),
+)
+
